f9f47f9819da2e221926748bf06ab38345fdcb89
Chris Madsen <cpmadsenviking@gmail.com>
2022-07-17 11:32:33 -0700

    Updated data on 2022-07-17 11:32:33

--- BlackberryMap/app.R
+++ BlackberryMap/app.R
@@ -5,33 +5,33 @@ library(leaflet)
 library(leaflet.extras)
 library(git2rdata)
 
-# dat = data.frame(location = "Sidney Airport - Stirling Way",
-#            productivity = "High")
-# 
-# dat$patch_number = nrow(dat)
-# 
-# dat = st_set_geometry(dat, st_geometry(st_cast(c(st_point(c(48.64726984376748, -123.40917029173556)),
-#   st_point(c(48.64734072836236, -123.40892352850621)),
-#   st_point(c(48.6468516226296, -123.40661682875333)),
-#   st_point(c(48.64527794591409, -123.40584435255703)),
-#   st_point(c(48.64468957669249, -123.40657391340909)),
-#   st_point(c(48.64468248785449, -123.40792574675264)),
-#   st_point(c(48.64599325820387, -123.40891219680323))), "POLYGON")))
-# 
-# dat = st_set_crs(dat, value = 4326)
-# 
-# my_geom = st_as_text(st_sfc(dat$geometry)) #Get geometry as text.
-# my_fields = st_drop_geometry(dat)
-# dat_to_write = cbind(my_fields, my_geom)
-# dat_to_write = dat_to_write %>%
-#     mutate(patch_number = row_number())
-# 
-# repo <- repository("F:/R Projects/blackberrymapping.github.io")
-# write_vc(dat_to_write, file = "blackberry_patches", 
-#          strict = F,
-#          root = repo, stage = TRUE)
-# commit(repo, all = T, message = paste0("Updated data on ",Sys.time()))
-# push(repo,credentials = git2r::cred_user_pass('cpmadsen', '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'))
+dat = data.frame(location = "Sidney Airport - Stirling Way",
+           productivity = "High")
+
+dat$patch_number = nrow(dat)
+
+dat = st_set_geometry(dat, st_geometry(st_cast(c(st_point(c(48.64726984376748, -123.40917029173556)),
+  st_point(c(48.64734072836236, -123.40892352850621)),
+  st_point(c(48.6468516226296, -123.40661682875333)),
+  st_point(c(48.64527794591409, -123.40584435255703)),
+  st_point(c(48.64468957669249, -123.40657391340909)),
+  st_point(c(48.64468248785449, -123.40792574675264)),
+  st_point(c(48.64599325820387, -123.40891219680323))), "POLYGON")))
+
+dat = st_set_crs(dat, value = 4326)
+
+my_geom = st_as_text(st_sfc(dat$geometry)) #Get geometry as text.
+my_fields = st_drop_geometry(dat)
+dat_to_write = cbind(my_fields, my_geom)
+dat_to_write = dat_to_write %>%
+    mutate(patch_number = row_number())
+
+repo <- repository("F:/R Projects/blackberrymapping.github.io")
+write_vc(dat_to_write, file = "blackberry_patches",
+         strict = F,
+         root = repo, stage = TRUE)
+commit(repo, all = T, message = paste0("Updated data on ",Sys.time()))
+push(repo,credentials = git2r::cred_user_pass('cpmadsen', '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'))
 
 # write_sf(dat, "blackberry_patches.gpkg")
 
