52acbaf562d82859da94436deeb4952e5cf29092
cpmadsen <cpmadsenviking@gmail.com>
2022-08-06 16:34:36 -0700

    added municipality polygons to leaflet map and updated UI to allow user to zoom map to their chosen municipality.

--- BlackberryMap/ui.R
+++ BlackberryMap/ui.R
@@ -14,8 +14,12 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      h3("Search for a patch"),
-      uiOutput('searchbar'),
+      h3("Zoom to Municipality"),
+      selectInput(inputId = "neighbourhood_selection",
+                  label = "",
+                  selected = 'All',
+                  selectize = F,
+                  choices = c('All',muns$ABRVN)),
       h3("Have a blackberry patch \n you'd like to add?"),
       h4("Find and draw it on the map, \nname it and classify it, \nthen click Add Patch!"),
       h3("Describe New Patch"),
